5a6e3a9bd7800d093d71082a6d9d80d8e66dfaaa
Zoran <zoran.mihelcic@gmail.com>
2016-01-31 19:23:16 +0000

    added widget backend form

--- includes/newsletter-subscriber-class.php
+++ includes/newsletter-subscriber-class.php
@@ -29,8 +29,27 @@ class Newsletter_Subscriber_Widget extends WP_Widget {
 	 */
 	public function form( $instance ) {
 		// outputs the options form on admin
+		$title = !empty($instance['title']) ? $instance['title'] : __('Newsletter Subscriber', 'ns_domain');
+		$recipient = $instance['recipient'];
+		$subject = !empty($instance['subject']) ? $instance['subject'] : __('You have a new Subscriber', 'ns_domain');
+		
+	
+	?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label><br>
+			<input type="text" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo esc_attr($title); ?>"/>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('recipient'); ?>"><?php _e('Recipient:'); ?></label><br>
+			<input type="text" id="<?php echo $this->get_field_id('recipient'); ?>" name="<?php echo $this->get_field_name('recipient'); ?>" value="<?php echo esc_attr($recipient); ?>"/>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('subject'); ?>"><?php _e('Subject:'); ?></label><br>
+			<input type="text" id="<?php echo $this->get_field_id('subject'); ?>" name="<?php echo $this->get_field_name('subject'); ?>" value="<?php echo esc_attr($subject); ?>"/>
+		</p>
+	
+	<?php
 	}
-
 	/**
 	 * Processing widget options on save
 	 *
